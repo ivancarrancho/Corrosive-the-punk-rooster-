960616246d0dddb19afb0216849ae076efa60223
Diego <djaramillop7@gmail.com>
2016-05-19 21:16:41 -0500

    responsive design menu v.1

--- User/urls.py
+++ User/urls.py
@@ -1,15 +1,17 @@
 # -*- encoding: utf-8 -*-
 from django.conf.urls import url
-from User.views import UserPageView, SuccessLoginView, SuccessSignUpView, SuccessLogoutView
+from User.views import UserPageView, SuccessLoginView, SuccessSignUpView, SuccessLogoutView, HomePageView
 # from django.contrib.auth import
 from . import views
 urlpatterns = [
-    url(r'^$', UserPageView.as_view(), name='user_list'),
+	url(r'^$', HomePageView.as_view(), name='home_view'),
+    url(r'^users', UserPageView.as_view(), name='user_list'),
     url(r'^success-signup', SuccessSignUpView.as_view(), name='success-signup'),
     url(r'^success-loguin', SuccessLoginView.as_view(), name='success-loguin'),
     url(r'^success-logout', SuccessLogoutView.as_view(), name='success-logout'),
     url(r'^login/$', views.authentication, name='authentication'),
     url(r'^create', views.create_data, name='create_data'),
     url(r'^signup', UserPageView.as_view(), name='signup'),
+    url(r'^home', HomePageView.as_view(), name='home_view'),
     
 ]
--- app/static/scripts/main.js
+++ app/static/scripts/main.js
@@ -1,14 +1,29 @@
 (function () {
+
+		   
 	
-  var loadCSS = require('./lib/loadCSS');
-  document.addEventListener('DOMContentLoaded', onDOMLoad);
+	
+	
+  	var loadCSS = require('./lib/loadCSS');
+  	document.addEventListener('DOMContentLoaded', onDOMLoad);
 
   
-  function onDOMLoad() {
+  	function onDOMLoad() {
     
-    loadCSS('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css');
-    loadCSS('https://fonts.googleapis.com/css?family=Montserrat|Lato');
+	    loadCSS('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css');
+	    loadCSS('https://fonts.googleapis.com/css?family=Montserrat|Lato');
     
+		var btnMenu= document.getElementById('btnMenu');
+		var buttonlistclass = document.getElementById('buttonlistclass');
+
+		btnMenu.addEventListener('click', onClickMenu);
+
+		function onClickMenu() {
+		  
+			buttonlistclass.classList.toggle('button-list');
+	  
+		}
+
 
   }
 }());
